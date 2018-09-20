f5202c5f4532485768017b049d823da689a89d5c
Ahammed K M <ahammed.km@gmail.com>
2018-09-21 04:51:22 +0530

    Assignment

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,12 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { User } from '../_models';
+
+@Injectable()
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    
+}
